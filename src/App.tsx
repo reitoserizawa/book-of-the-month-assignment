0a67c9c0e985dab6af89eb9642bbeb22da261b2f
ReitoSerizawa <s.reitiger@gmail.com>
2026-05-05 00:15:14 -0400

    delete App component

--- src/App.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import CheckoutPage from './components/pages/CheckoutPage';
-
-const App: React.FC = () => {
-    return <CheckoutPage />;
-};
-
-export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -4,8 +4,8 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 import './index.css';
 
-import App from './App';
 import ErrorBoundary from './components/ErrorBoundary';
+import CheckoutPage from './components/pages/CheckoutPage';
 
 const queryClient = new QueryClient();
 
@@ -13,7 +13,7 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
         <QueryClientProvider client={queryClient}>
             <ErrorBoundary>
-                <App />
+                <CheckoutPage />
             </ErrorBoundary>
         </QueryClientProvider>
     </React.StrictMode>,
